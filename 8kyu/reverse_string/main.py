34dbd2f011b763b43cd43b42b4e51987ef234ba1
dpillay03 <dpillay03@gmail.com>
2019-11-17 22:29:10 -0800

    finished reverse string algorithm

---
+++ 8kyu/reverse_string/main.py
@@ -0,0 +1,10 @@
+# Complete the solution so that 
+# it reverses the string value passed into it.
+
+def solution(string):
+    str = "" 
+    for i in string: 
+      str = i + str
+    return str
+
+    
