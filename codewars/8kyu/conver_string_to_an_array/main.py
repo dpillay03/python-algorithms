359ec71e2bcfee03463e306b73cb904e0602b6b1
DANIEL PILLAY <DPILLAY03@GMAIL.COM>
2019-12-16 08:43:23 -0800

    finished convert string

---
+++ codewars/8kyu/conver_string_to_an_array/main.py
@@ -0,0 +1,8 @@
+"""
+"Robin Singh" ==> ["Robin", "Singh"]
+
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+"""
+def string_to_array(s):
+    split = s.split(" ")
+    return split
